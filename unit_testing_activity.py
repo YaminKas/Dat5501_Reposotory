e5f7f316dcc1c0fb448356b1320a77cc376c353c
YaminKas <yaminkashim@gmail.com>
2025-09-25 15:08:28 +0100

    unit_test_activity

---
+++ unit_testing_activity.py
@@ -0,0 +1,12 @@
+#function that adds two numbers
+
+#what would the requirements be?
+#only take numbers as an input i.e. it would always be a float
+
+
+def addition(x,y):
+    sum = (x) + (y)
+    print(sum)
+    return sum
+
+addition(input('enter number 1 to add: '), input('enter number 2 to add: '))

4f862ac35ed9498a9ed01340d2bb6b8f7401e8ec
YaminKas <yaminkashim@gmail.com>
2025-09-25 15:20:51 +0100

    new changes

--- actual_unit_test.py
+++ actual_unit_test.py
@@ -1,5 +1,20 @@
 import unittest
-from unit_testing_activity import addition,run
+from unit_testing_activity import run,addition
 
-run()
+
+
+class testing_cases(unittest.TestCase):
+    def test_add(self):
+
+        self.assertEqual(addition(2,3),5)
+
+        self.assertEqual(addition(-6,3),-3)
+
+        self.assertEqual(addition(3.1,5.4), 8.5)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
+
 

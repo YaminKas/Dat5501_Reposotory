71d2c567ddc49bb703008dc1e79b7e0f71396133
YaminKas <yaminkashim@gmail.com>
2025-09-29 12:54:08 +0100

    commit 2

--- compound_interest_calculator.py
+++ compound_interest_calculator.py
@@ -4,7 +4,7 @@ def comp_int_calc():
     starting_amount = int(input('What is the starting amount?: '))
     #contributing_amount = int(input('What is the amount you will be contributing monthly?: '))
     #contributing_timeframe = int(input('What is the '))
-    #length_of_time = int(input('What is the length of time you would like to model?: '))
+    #length_of_time = int(input('What is the length of time you would like to model?: ')) 
     interest_rate_per_year = int(input('What is the interest rate per year?: '))
     interest_multiplier = (interest_rate_per_year/100)+1
     x = starting_amount
---
+++ test_suite.py
@@ -0,0 +1,3 @@
+from compound_interest_calculator import comp_int_calc
+
+comp_int_calc()

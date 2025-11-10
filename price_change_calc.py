4def27966ed039e56fcfd74c621375ed4bffbec2
YaminKas <yaminkashim@gmail.com>
2025-11-10 11:13:13 +0000

    second commit

--- price_change_calc.py
+++ price_change_calc.py
@@ -1,8 +1,13 @@
 #this calculator will calculate the change in a stocks price
 
-arrray = [0,1,2,3,4,5,6,7,8,9,]
+array = [0,1,2,3,4,5,6,7,8,9,]
 
 def calc(array):
     len_array = len(array)
     for x in range (0 , (len_array-1) ):
-        return array[x] + array[x+1]
+        print( array[x+1] - array[x] )
+    
+calc(array)
+
+
+# order will be set as n-1

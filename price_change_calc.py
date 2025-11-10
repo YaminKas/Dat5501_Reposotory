252c412d076b480c56825bfcf70ac5d20e248d75
YaminKas <yaminkashim@gmail.com>
2025-11-10 15:13:10 +0000

    seaching for neg 0

--- price_change_calc.py
+++ price_change_calc.py
@@ -1,4 +1,19 @@
-#this calculator will calculate the change in a stocks price
+#this calculator will calculate the change in price
+array = [0,1,2,3,4,5,6,7,8,9,]
+
+def calc(array):
+    len_array = len(array)
+    for x in range (0 , (len_array-1) ):
+        print( array[x+1] - array[x] )
+    
+#calc(array)
+
+
+# order will be set as n-1
+
+
+# the code below this take in the price data of EURUSD and then retunr the price differnce for 11/24 - 11/25
+# it return the difference to 3 decimal places and whether the movement was a rise or fall in price.
 
 import pandas as pd
 
@@ -24,16 +39,7 @@ for index, row in df.iterrows():
 df["Difference"] = differences
 df["Movement"] = movements
 
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 print(df)
-
-array = [0,1,2,3,4,5,6,7,8,9,]
-
-def calc(array):
-    len_array = len(array)
-    for x in range (0 , (len_array-1) ):
-        print( array[x+1] - array[x] )
-    
-#calc(array)
-
-
-# order will be set as n-1
